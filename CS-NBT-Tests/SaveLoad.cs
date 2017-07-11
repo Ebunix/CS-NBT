6bca8c351d59ac0efc1bd477a54775053fe83250
Ebunix <simonelberich@gmail.com>
2017-07-11 21:36:22 +0200

    Assembly info update

--- CS-NBT-Tests/SaveLoad.cs
+++ CS-NBT-Tests/SaveLoad.cs
@@ -12,19 +12,53 @@ namespace CS_NBT_Tests
         [TestMethod]
         public void TestSaveEmpty()
         {
-            if (File.Exists("test.nbt"))
-                File.Delete("test.nbt");
+            RemoveTestFile();
 
             NbtCompound expected = new NbtCompound();
-            
-            using (NbtWriter w = new NbtWriter(File.Open("test.nbt", FileMode.Create)))
+            using (NbtWriter w = OpenTestFile())
                 w.SaveTree(expected);
 
             NbtCompound loaded;
-            using (NbtReader r = new NbtReader(File.Open("test.nbt", FileMode.Open)))
+            using (NbtReader r = ReadTestFile())
                 loaded = r.GetTree();
 
             Assert.AreEqual(expected, loaded);
         }
+
+        [TestMethod]
+        public void TestSaveCompound()
+        {
+            NbtCompound expected = new NbtCompound();
+            expected.Name = "RootCompound";
+            expected.Attach(new NbtString { Name = "TestString", Value = "Test Value 123" });
+
+            using (NbtWriter w = OpenTestFile())
+                w.SaveTree(expected);
+
+            NbtCompound loaded;
+            using (NbtReader r = ReadTestFile())
+                loaded = r.GetTree();
+
+            Assert.AreEqual(expected, loaded);
+        }
+
+    //  Helpers
+    // =========
+
+    private static NbtReader ReadTestFile()
+    {
+        return new NbtReader(File.Open("test.nbt", FileMode.Open));
     }
+
+    private static NbtWriter OpenTestFile()
+    {
+        return new NbtWriter(File.Open("test.nbt", FileMode.Create));
+    }
+
+    private static void RemoveTestFile()
+    {
+        if (File.Exists("test.nbt"))
+            File.Delete("test.nbt");
+    }
+}
 }
