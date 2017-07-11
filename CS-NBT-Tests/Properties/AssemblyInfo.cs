f324fefbe7a660666ea448c660d43f33899b3914
Ebunix <simonelberich@gmail.com>
2017-07-11 18:59:05 +0200

    Added unit test project

---
+++ CS-NBT-Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CS-NBT-Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CS-NBT-Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("beaaa058-1bd5-4640-b9a5-40aa69f8762a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CS-NBT-Tests/SaveLoad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nbtlib;
+using nbtlib.Nodes;
+
+namespace CS_NBT_Tests
+{
+    [TestClass]
+    public class SaveLoad
+    {
+        [TestMethod]
+        public void TestSaveEmpty()
+        {
+            if (File.Exists("test.nbt"))
+                File.Delete("test.nbt");
+
+            NbtCompound expected = new NbtCompound();
+            
+            using (NbtWriter w = new NbtWriter(File.Open("test.nbt", FileMode.Create)))
+                w.SaveTree(expected);
+
+            NbtCompound loaded;
+            using (NbtReader r = new NbtReader(File.Open("test.nbt", FileMode.Open)))
+                loaded = r.GetTree();
+
+            Assert.AreEqual(expected, loaded);
+        }
+    }
+}
